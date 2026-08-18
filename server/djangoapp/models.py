f585eee8f3057c61d5bc7baed2a90bb624658a17
Jessie D'Andrea <jessiedandrea@Jessies-MacBook-Pro.local>
2026-08-17 22:58:05 -0400

    Add flake8 config and fix all linting errors

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -6,36 +6,37 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
+
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
 
     def __str__(self):
-        return self.name # Return the name as the string representation
+        return self.name  # Return the name as the string representation
+
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE) # Many-to-one relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-one relationship
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('HATCHBACK', 'Hatchback'),
         ('CROSSOVER', 'Crossover'),
-        ('COUPE','Coupe'),
-        ('COUPE SUV','Coupe SUV'),
+        ('COUPE', 'Coupe'),
+        ('COUPE SUV', 'Coupe SUV'),
         ('SUV', 'SUV'),
-        ('PICK-UP','Pick-up'),
+        ('PICK-UP', 'Pick-up'),
         ('WAGON', 'Wagon'),
-        ('VAN','Van'),
-        ('SPORT','Sport'),
-        ('MUSCLE','Muscle'),
+        ('VAN', 'Van'),
+        ('SPORT', 'Sport'),
+        ('MUSCLE', 'Muscle'),
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-        validators=[
-            MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ])
+                               validators=[
+                                   MaxValueValidator(2023),
+                                   MinValueValidator(2015)
+                               ])
 
     def __str__(self):
-        return self.name # Return the name as the string representation
-
+        return self.name  # Return the name as the string representation
